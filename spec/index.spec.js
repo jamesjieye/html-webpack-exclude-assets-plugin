0df181f638ea6787f4d08b4769812f9590002624
James Ye <james.ye@hotmail.com>
2016-11-21 11:40:57 +0800

    Init commit

---
+++ spec/index.spec.js
@@ -0,0 +1,114 @@
+/* eslint-env jasmine */
+var path = require('path');
+var fs = require('fs');
+var cheerio = require('cheerio');
+var webpack = require('webpack');
+var rm_rf = require('rimraf');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var HtmlWebpackExcludeAssetsPlugin = require('../');
+
+var OUTPUT_DIR = path.join(__dirname, '../dist');
+
+describe('HtmlWebpackExcludeAssetsPlugin', function () {
+  beforeEach(function (done) {
+    rm_rf(OUTPUT_DIR, done);
+  });
+
+  it('should not exclude assets by default', function (done) {
+    webpack({
+      entry: {
+        app: path.join(__dirname, 'fixtures', 'entry.js'),
+        style: [path.join(__dirname, 'fixtures', 'app.css')]
+      },
+      output: {
+        path: OUTPUT_DIR,
+        filename: '[name].js'
+      },
+      module: {
+        loaders: [{ test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader') }]
+      },
+      plugins: [
+        new ExtractTextPlugin('[name].css'),
+        new HtmlWebpackPlugin(),
+        new HtmlWebpackExcludeAssetsPlugin()
+      ]
+    }, function (err) {
+      expect(err).toBeFalsy();
+      var htmlFile = path.resolve(__dirname, '../dist/index.html');
+      fs.readFile(htmlFile, 'utf8', function (er, data) {
+        expect(er).toBeFalsy();
+        var $ = cheerio.load(data);
+        expect($('script[src="style.js"]').toString()).toBe('<script type="text/javascript" src="style.js"></script>');
+        expect($('link[href="style.css"]').toString()).toBe('<link href="style.css" rel="stylesheet">');
+        done();
+      });
+    });
+  });
+
+  it('should exclude assets when regex matches asset file names', function (done) {
+    webpack({
+      entry: {
+        app: path.join(__dirname, 'fixtures', 'entry.js'),
+        style: [path.join(__dirname, 'fixtures', 'app.css')]
+      },
+      output: {
+        path: OUTPUT_DIR,
+        filename: '[name].js'
+      },
+      module: {
+        loaders: [{ test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader') }]
+      },
+      plugins: [
+        new ExtractTextPlugin('[name].css'),
+        new HtmlWebpackPlugin({
+          excludeAssets: [/style.*.js/]
+        }),
+        new HtmlWebpackExcludeAssetsPlugin()
+      ]
+    }, function (err) {
+      expect(err).toBeFalsy();
+      var htmlFile = path.resolve(__dirname, '../dist/index.html');
+      fs.readFile(htmlFile, 'utf8', function (er, data) {
+        expect(er).toBeFalsy();
+        var $ = cheerio.load(data);
+        expect($('script[src="style.js"]').toString()).toBe('');
+        expect($('link[href="style.css"]').toString()).toBe('<link href="style.css" rel="stylesheet">');
+        done();
+      });
+    });
+  });
+
+  it('should exclude assets when only use one regex instead of an array', function (done) {
+    webpack({
+      entry: {
+        app: path.join(__dirname, 'fixtures', 'entry.js'),
+        style: [path.join(__dirname, 'fixtures', 'app.css')]
+      },
+      output: {
+        path: OUTPUT_DIR,
+        filename: '[name].js'
+      },
+      module: {
+        loaders: [{ test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader') }]
+      },
+      plugins: [
+        new ExtractTextPlugin('[name].css'),
+        new HtmlWebpackPlugin({
+          excludeAssets: /style.*.js/
+        }),
+        new HtmlWebpackExcludeAssetsPlugin()
+      ]
+    }, function (err) {
+      expect(err).toBeFalsy();
+      var htmlFile = path.resolve(__dirname, '../dist/index.html');
+      fs.readFile(htmlFile, 'utf8', function (er, data) {
+        expect(er).toBeFalsy();
+        var $ = cheerio.load(data);
+        expect($('script[src="style.js"]').toString()).toBe('');
+        expect($('link[href="style.css"]').toString()).toBe('<link href="style.css" rel="stylesheet">');
+        done();
+      });
+    });
+  });
+});
